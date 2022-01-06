bea17d3ca16945d08f1dcecc5b61a1d099307712
William Robersone <wmrobersone@yahoo.com>
2022-01-06 10:52:09 -0600

    added search page and filters

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { Flex, Select, Box, Input, Text, Spinner, Icon, Button } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { MdCancel } from "react-icons/md";
+import Image from "next/image";
+import { filterData, getFilterValues } from "../utils/filterData";
+
+const SearchFilters = () => {
+    const [filters, setFilters] = useState(filterData);
+    const searchProperties = (filterValues) => {
+
+    }
+
+    return (
+        <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
+            {filters.map((filter) => (
+                <Box key={filter.queryName}>
+                    <Select
+                        placeholder={filter.placeholder}
+                        onChange={(e) => searchProperties({ [filter.queryName]: e.target.value })}
+                        w="fit-content"
+                        p="2"
+                    >
+                        {filter?.items?.map((item) => (
+                            <option value={item.value} key={item.value}>{item.name}</option>
+                        ))}
+                    </Select>
+                </Box>
+            ))}
+        </Flex>
+    )
+}
+
+export default SearchFilters;
